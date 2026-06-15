66f91e8c9d00b072c02f5886c3ee2387f4929f48
MatheusRezendev <contatorzn@hotmail.com>
2026-06-14 22:55:42 -0300

    refactor(producer): migrate from useProducerData to direct service call

--- src/pages/ProducerHistory/index.tsx
+++ src/pages/ProducerHistory/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { View, Text, FlatList } from 'react-native';
 import { ScreenHeader, Card, Divider, Volume, NumText, ColetaRow } from '../../components';
-import { useProducerData } from '../../services/useProducerData';
+import { CURRENT_PRODUCER_ID, loadProducerData } from '../../services/producerService';
 import { styles } from './styles';
 
 export function ProducerHistoryPage() {
-  const data = useProducerData();
+  const data = loadProducerData(CURRENT_PRODUCER_ID);
 
   if (!data) {
     return <View style={styles.historyContainer} />;
