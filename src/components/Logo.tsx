07dc412f6d96ee09d1fca774a9add209924775ea
MatheusRezendev <contatorzn@hotmail.com>
2026-06-14 22:45:09 -0300

    feat(icons): replace text placeholders with SVG icons and add definitive logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Image as RNImage, ImageStyle } from 'react-native';
+
+interface LogoProps {
+  size?: number;
+  style?: ImageStyle;
+}
+
+const WIDTH = 630;
+const HEIGHT = 851;
+const RATIO = WIDTH / HEIGHT;
+
+export function Logo({ size = 156, style }: LogoProps) {
+  return (
+    <RNImage
+      source={require('../../assets/images/logo.png')}
+      style={[
+        {
+          width: size,
+          height: size / RATIO,
+          resizeMode: 'contain',
+        },
+        style,
+      ]}
+    />
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,15 @@
+export { Avatar } from './Avatar';
 export { Card } from './Card';
 export { ColetaRow } from './ColetaRow';
 export { CowMark } from './CowMark';
+export { Logo } from './Logo';
 export { Divider } from './Divider';
+export { Field } from './Field';
+export { FilterPill } from './FilterPill';
 export { MoneyBRL } from './MoneyBRL';
 export { NumText } from './NumText';
+export { OfflineBanner } from './OfflineBanner';
+export { PerfilPlaceholder } from './PerfilPlaceholder';
 export { PhotoStripe } from './PhotoStripe';
 export { ProjectionCard } from './ProjectionCard';
 export { ScreenHeader } from './ScreenHeader';
@@ -19,6 +25,15 @@ export {
   WalletIcon,
   CheckIcon,
   InfoIcon,
+  PlusIcon,
+  UsersIcon,
+  CameraIcon,
+  SearchIcon,
+  EditIcon,
+  ChevronIcon,
+  RouteIcon,
+  TruckIcon,
+  LogoutIcon,
 } from './icons/Icon';
 export type { IconProps } from './icons/Icon';
 export { MailIcon, LockIcon, AlertIcon } from './AuthIcons';
